f7252035d50e0ec549098b7816d82a03aaf27f6d
Lauréna Porcel <lunaelfyr@yahoo.fr>
2021-03-30 21:59:29 +0200

    add new home page

--- src/main/webapp/home.js
+++ src/main/webapp/home.js
@@ -14,6 +14,16 @@ function deleteServerData(url, success){
     }).done(success);
 }
 
+function putServerData(url, data, success){
+    $.ajax({
+		type: "PUT",
+		contentType: "application/json; charset=utf-8",
+        dataType: "json",
+		data: data,
+        url: url
+    }).done(success);
+}
+
 function callDone(result){
 	var templateExample = _.template($('#templateExample').html());
 
