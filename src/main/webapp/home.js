51ee5710a17bb8bf334b11eb40db76ef9260c3e0
reddragon214 <62596248+reddragon214@users.noreply.github.com>
2021-04-07 03:33:39 +0200

    Add the beginning of the javascript implementation
    J'ai ajouté le début du javascript sur le home, ainsi que des pages html bouchon pour le login/sign up/help.
    Ce rendu ne tient pas compte de la dernière version de la home page et de la page login.

--- src/main/webapp/home.js
+++ src/main/webapp/home.js
@@ -1,4 +1,20 @@
 
+$( function() {
+  $( "#datepicker" ).datepicker();
+} );
+
+$( function() {
+  var availableTags = [
+    "AerodromeA",
+    "AerodromeB",
+    "AerodromeC",
+    "AerodromeD",
+  ];
+  $( "#tags" ).autocomplete({
+    source: availableTags
+  });
+} );
+
 function getServerData(url, success){
     $.ajax({
         dataType: "json",
@@ -42,4 +58,33 @@ $(function(){
 		deleteServerData("ws/users/1", callDone);
 	});
 	
+});
+
+function fillTable(container) {
+    var template = _.template($('#templateRow').html());
+    var result = "";
+
+    container.actions.forEach(action => result += template(action));
+
+    $("#result").html(result);
+}
+
+
+$(function () {
+  $("#buttonSearch").click(function () {
+    var id = $("#tags").val();
+    var date = $("#datepicker").val();
+    var myDate = date.split("/");
+    var newDate = myDate[2] +"-"+ myDate[1]+"-"+myDate[1];
+    if (id == '') {
+        getServerData("ws/flights/", fillTable);
+    }
+    else if ( myDate[1]==undefined ||  myDate[2]==undefined ||  myDate[0]==undefined) {
+        getServerData("ws/flights/search/" + id, fillTable);
+    }
+    else{
+        getServerData("ws/flights/" + id +"/"+ newDate, fillTable);
+    }
+    window.location.href="research.html";
+  });
 });
