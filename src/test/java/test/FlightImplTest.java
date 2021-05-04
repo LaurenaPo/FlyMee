9cca8e227330ccc0123e49354e673f104e81b84b
macabcs <62359346+macabcs@users.noreply.github.com>
2021-05-05 00:15:11 +0200

    Update FlightImplTest.java

--- src/test/java/test/FlightImplTest.java
+++ src/test/java/test/FlightImplTest.java
@@ -42,63 +42,48 @@ public class FlightImplTest {
 		
 		Pilot p1 = new Pilot(1, "Jean", "Michel","jm@mail.fr", LocalDate.of(1950, 10, 10),"mdp", "photo","pilote chevronne",85, "20 ans", "licence tourisme", 450, "0605040302");
 		Pilot p2 = new Pilot(2, "Jean", "Francois","jf@mail.fr", LocalDate.of(1950, 2, 10),"mdp", "photo","pilote philanthrope",85, "2 ans", "licence 4", 21, "0605050302");
-		Pilot p3 = new Pilot(1, "Jean", "Mouloud","jmou@mail.fr", LocalDate.of(1950, 1, 10),"mdp", "photo","pirate de l'air",72, "13 ans", "licence tourisme", 202, "0605040303");
+		Pilot p3 = new Pilot(4, "Jean", "Mouloud","jmou@mail.fr", LocalDate.of(1950, 1, 10),"mdp", "photo","pirate de l'air",72, "13 ans", "licence tourisme", 202, "0605040303");
 		User u1 = new User(1, "Jean", "Truc","jt@mail.fr", LocalDate.of(1990, 10, 10),"mdp");
-		User u2 = new User(1, "Machin", "Truc","mt@mail.fr", LocalDate.of(1991, 10, 10),"mdp");
-		User u3 = new User(1, "Fred", "Truc","ft@mail.fr", LocalDate.of(1980, 10, 10),"mdp");
-		User u4 = new User(1, "Pierre", "Pierre","pp@mail.fr", LocalDate.of(1900, 10, 10),"mdp");
-		Aircraft aircraft1 =new Aircraft(1,,10,(long) 1520);
-		Aircraft aircraft2 =new Aircraft("PHOENIX40","Boeing",10,(long) 4213);
-		Aircraft aircraft3 =new Aircraft("PHOENIX44","Boeing",10,(long) 1597);
-		Long aircraftId1=aircraft1.getTailNumber();
-		Long aircraftId2=aircraft2.getTailNumber();
+		User u2 = new User(2, "Machin", "Truc","mt@mail.fr", LocalDate.of(1991, 10, 10),"mdp");
+		User u3 = new User(3, "Fred", "Truc","ft@mail.fr", LocalDate.of(1980, 10, 10),"mdp");
+		User u4 = new User(4, "Pierre", "Pierre","pp@mail.fr", LocalDate.of(1900, 10, 10),"mdp");
+		User u5 = new User(5, "Madame", "Monsieur","mm@mail.fr", LocalDate.of(1978, 10, 10),"mdp");
+		
+		
+		Aircraft aircraft1 =new Aircraft(1, p1, "Cessna 340", 4);
+		Aircraft aircraft2 =new Aircraft(2, p3, "Cessna 320", 2);
+		
 
-		Pilot pilot1=new Pilot("Rayan","Gosling","rayan@mail.fr","07XXX",new Date(1980,03,16),200);
-		Pilot pilot2=new Pilot("Harry","Potter","potter@mail.fr","07XXX",new Date(1988,12,16),200);
-		Pilot pilot3=new Pilot("Scarlett","Johanson","sjohan@mail.fr","07XXX",new Date(1983,12,16),200);
-		Pilot pilot4=new Pilot("Tristan","Ottaviano","ottaviano@mail.fr","07XXX",new Date(1999,11,18),50);
+			
 		
-		Passenger passenger1=new Passenger("Hiba","SOUBER","hiba@mail.fr","06XXX",new Date(1999,06,12));
-		Passenger passenger2=new Passenger("Othmane","SOUBER","othmane@mail.fr","07XXX",new Date(1996,02,8));
-		Passenger passenger3=new Passenger("Saadia","SOUBER","saadia@mail.fr","08XXX",new Date(1971,06,10));
-		Passenger passenger4=new Passenger("Quentin","TARANTINO","quentin@mail.fr","03XXX",new Date(1963,03,27));
-		Passenger passenger5=new Passenger("Stanley","KUBRICK","stan@mail.fr","09XXX",new Date(1928,07,26));
-		Passenger passenger6=new Passenger("Naruto","Uzumaki","naruto@mail.fr","02XXX",new Date(1999,07,23));	
+		aidi.addAircraft(aircraft1);
+		aidi.addAircraft(aircraft2);
+		Assert.assertEquals(2, aidi.getAircrafts().size());
+		aidi.deleteAircraft(2);
+		Assert.assertEquals(1, aidi.getAircrafts().size());	
 		
-		aircraftDao.addAircraft(aircraft1);
-		aircraftDao.addAircraft(aircraft2);
-		aircraftDao.addAircraft(aircraft3);
-		Assert.assertEquals(3, aircraftDao.getFleet().size());
-		aircraftDao.deleteAircraft((long) 2);
-		Assert.assertEquals(2, aircraftDao.getFleet().size());	
+		pdi.addPilot(p1);
+		pdi.addPilot(p2);
+		pdi.addPilot(p3);
+		Assert.assertEquals(3, pdi.getPilots().size());
+		pdi.deletePilot(2);
+		Assert.assertEquals(2, pdi.getPilots().size());	
 		
-		pilotDao.addPilot(pilot1);
-		pilotDao.addPilot(pilot2);
-		pilotDao.addPilot(pilot3);
-		pilotDao.addPilot(pilot4);
-		Assert.assertEquals(4, pilotDao.getPilots().size());
-		pilotDao.deletePilot((long) 3);
-		Assert.assertEquals(3, pilotDao.getPilots().size());	
+		pdi.updatePilot(new Pilot(1, "Jean", "Michel","jm@mail.fr", LocalDate.of(1950, 10, 10),"mdp", "photo","pilote avec vertige",85, "20 ans", "licence tourisme", 450, "0605040302"));
 		
-		pilotDao.modifyPwd((long) 0,"apple");
-		pilotDao.modifyUsername((long) 0,"banana");
+		System.out.print(pdi.getPilots().get(0).getDescription());
 		
-		pilotDao.getPilots().get(0).display();
+	
 		
-		// test login
-		Pilot p= pilotDao.Login("banana","apple");
-		Assert.assertEquals("Rayan", p.getName());
+		udi.addUser(u1);
+		udi.addUser(u2);
+		udi.addUser(u3);
+		udi.addUser(u4);
+		udi.addUser(u5);
 		
-		passengerDao.addPassenger(passenger1);
-		passengerDao.addPassenger(passenger2);
-		passengerDao.addPassenger(passenger3);
-		passengerDao.addPassenger(passenger4);
-		passengerDao.addPassenger(passenger5);
-		passengerDao.addPassenger(passenger6);
-		
-		Assert.assertEquals(6,passengerDao.getPassengers().size());
-		passengerDao.deletePassenger((long)5);
-		Assert.assertEquals(5,passengerDao.getPassengers().size());	
+		Assert.assertEquals(5,udi.getUsers().size());
+		udi.deleteUser(2);
+		Assert.assertEquals(5,udi.getUsers().size());	
 		
 		Flight flight1 = new Flight(new Date(2021,03,16),LocalDateTime.of(2021,03,16,18,20,0), "Aerodrome 1",new Date(2021,03,16),LocalDateTime.of(2021,03,16,20,50,0),"Aerodrom2",150,37,pilot1.getPilotId(),aircraft1.getTailNumber());	
 		Flight flight2 = new Flight(new Date(2021,03,16),LocalDateTime.of(2021,03,16,18,20,0), "Aerodrome 1",new Date(2021,03,16),LocalDateTime.of(2021,03,16,20,50,0),"Aerodrom3",30,48,pilot2.getPilotId(),aircraft2.getTailNumber());		
